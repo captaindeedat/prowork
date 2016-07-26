4f4166e54df927e5bc968fa3962f63b74bcdfdf9
Ahmed Opeyemi <captaindeedat@gmail.com>
2016-07-26 11:09:31 +0100

    camelCase
    This is the camelCase program

---
+++ prowork/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prowork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p;
+            char q;
+            int count = 0;
+            Console.WriteLine("Enter the string");
+            p = Console.ReadLine();
+           
+            foreach (char t in p)
+            {
+                
+                q = char.ToUpper(t);
+
+                if (t == q)
+                {
+                    count += 1;
+                    
+                }
+               
+            }
+            Console.WriteLine(count+1);
+            Console.ReadLine();
+        }
+    }
+}
